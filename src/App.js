2babf69dbe16fc7312afedc8fbfc614e317d241c
anveshdange <anveshdange19@gmail.com>
2021-10-06 11:16:40 +0530

    My first react app on github pages

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>( Made by Anvesh Dange )</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://bit.ly/anveshdange"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          My Personal Portfolio (Click Me)
         </a>
       </header>
     </div>
